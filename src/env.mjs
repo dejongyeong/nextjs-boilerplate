b0e349a6b196905f0b440e5ba85b3cbe198387c1
De Jong Yeong <dejong.re2@gmail.com>
2023-10-24 16:15:05 +0100

    feat: typesafe env variables with t3 env and zod

---
+++ src/env.mjs
@@ -0,0 +1,33 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  /**
+   * Specify your server-side environment variables schema here. This way you can ensure the app
+   * isn't built with invalid env vars.
+   */
+  server: {
+    DATABASE_URL: z.string().url(),
+    OPEN_AI_API_KEY: z.string().min(1),
+  },
+
+  /**
+   * Environment variables available on the client (and server).
+   * You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
+   */
+  client: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+  },
+
+  // For Next.js >= 13.4.4, you only need to destructure client variables:
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+  },
+
+  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
+  //   runtimeEnv: {
+  //     DATABASE_URL: process.env.DATABASE_URL,
+  //     OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+  //     NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+  //   },
+});
